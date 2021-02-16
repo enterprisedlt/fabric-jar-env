0d0ca52a741fbd9c788b02ce3e214efbcbf90b8e
Andrew Pudovikov <pudovikov@me.com>
2021-02-16 18:29:04 +0300

    jarenv version upgrade up to latest fabric build

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t enterprisedlt/fabric-jar-env:1.4.1 .
-docker tag enterprisedlt/fabric-jar-env:1.4.1 enterprisedlt/fabric-jar-env
+docker build -t enterprisedlt/fabric-jar-env:1.4.8 .
+docker tag enterprisedlt/fabric-jar-env:1.4.8 enterprisedlt/fabric-jar-env
